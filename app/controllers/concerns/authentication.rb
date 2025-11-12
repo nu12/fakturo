a5607b6d347cc2604d2932528e56db151c5236b0
nu12 <34694287+nu12@users.noreply.github.com>
2025-11-12 00:12:05 -0500

    Prevent SQL injection

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -19,8 +19,8 @@ module Authentication
 
     def current_user
       return Current.user if authenticated?
-      permitted = params.expect(user: [ :uuid, :access_token ]
-      user = User.where(access_token_expiry_date: Time.now..DateTime::Infinity.new).find_by(permitted).merge({ access_token_enabled: true }))
+      params.expect(:uuid, :access_token)
+      user = User.where(access_token_expiry_date: Time.now..DateTime::Infinity.new).find_by(uuid: params[:uuid], access_token: params[:access_token], access_token_enabled: true)
       user || User.new
     end
 
