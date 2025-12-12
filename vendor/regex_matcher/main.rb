35a9ca51284130283594236e3904398888224017
nu12 <34694287+nu12@users.noreply.github.com>
2025-12-12 01:18:56 -0500

    Add second regex

--- vendor/regex_matcher/main.rb
+++ vendor/regex_matcher/main.rb
@@ -50,9 +50,26 @@ class RegexMatcherServer
     lines = text.gsub(/ +/, " ").gsub("\r\n", "\n").split("\n")
     lines.each do |line|
       m = /(?<day>\d{2})\W(?<month>\d{2})\W\d{2}\W\d{2}\W(?<description>[\w' ]+).+\D(?<value>\d+.\d{2}|\d+.\d{2}CR)\s*$/.match(line)
-      next unless m
-      if m[:day] && m[:month] && m[:description] && m[:value]
+      n = /^\s*(?<month>\S{3,})\s(?<day>\d{2})\s+\S{3,}\s\d{2}[\s\W]+(?<description>[\w' ]+).*\D(?<value>\d+,\d{2})/.match(line.force_encoding(Encoding::ISO_8859_1))
+      next unless m || n
+      if m && m[:day] && m[:month] && m[:description] && m[:value]
         result << {date: "#{Date.today.year}-#{m[:month]}-#{m[:day]}", description: m[:description], value: m[:value].to_f}
+      elsif n && n[:day] && n[:month] && n[:description] && n[:value]
+        months = {
+          "jan" => "01",
+          "fév" => "02",
+          "mar" => "03",
+          "avr" => "04",
+          "mai" => "05",
+          "jun" => "06",
+          "jlt" => "07",
+          "aoû" => "08",
+          "sep" => "09",
+          "oct" => "10",
+          "nov" => "11",
+          "déc" => "12"
+        }
+        result << {date: "#{Date.today.year}-#{months[n[:month].force_encoding(Encoding::UTF_8)]}-#{n[:day]}", description: n[:description], value: n[:value].gsub(",", ".").to_f}
       end
     end
     return result
