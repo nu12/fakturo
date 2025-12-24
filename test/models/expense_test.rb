d34fd54f87e6d2c8fed3ce7d40e032e8bf8ab28b
nu12 <34694287+nu12@users.noreply.github.com>
2025-12-23 21:27:57 -0500

    WIP

--- test/models/expense_test.rb
+++ test/models/expense_test.rb
@@ -39,4 +39,13 @@ class ExpenseTest < ActiveSupport::TestCase
     assert es[1].name == "SubThree"
     assert es[1].value == 13.03
   end
+
+  test "adjusted year" do
+    st = statements(:one)
+    cat, sub = user.uncategorized
+    same_year = Expense.create("2020-01-01", description: "Same year", value: 92.11, statement: st, user: st.user, category: cat, subcategory: sub)
+    assert_equal(2020, same_year.date.year)
+    last_year = Expense.create("2020-12-20", description: "Last year", value: 528.81, statement: st, user: st.user, category: cat, subcategory: sub)
+    assert_equal(2019, last_year.date.year)
+  end
 end
