76269e1ba5f11501a188622ede53d2d372630d37
nu12 <34694287+nu12@users.noreply.github.com>
2025-12-24 16:32:36 -0500

    Auto category feature

--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -1,2 +1,6 @@
 module ExpensesHelper
+  def self.auto_find_category expense
+    relatable = expense.user.expenses.where(raw_description: expense.raw_description).first
+    expense.update(category: relatable.category, subcategory: relatable.subcategory)
+  end
 end
