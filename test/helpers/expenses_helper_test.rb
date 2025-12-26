6543f08bbf29012c662f83e078d667c17b1b70bf
nu12 <34694287+nu12@users.noreply.github.com>
2025-12-25 23:12:26 -0500

    Auto category with last instead of first

--- test/helpers/expenses_helper_test.rb
+++ test/helpers/expenses_helper_test.rb
@@ -4,12 +4,17 @@ class ExpensesHelperTest < ActiveSupport::TestCase
   test "find category" do
     us, ca, su, so, st, ex = ActiveSupport::Helper.bootstrap
     uncat, unsub = us.uncategorized
-    e = Expense.create(date: "2020-01-12", raw_description: "autocat", description: "Any", value: 12.98, statement: st, user: us, category: uncat, subcategory: unsub)
-    assert_equal("Uncategorized", e.category.name)
-    assert_equal("Uncategorized", e.subcategory.name)
+    e1 = Expense.create(date: "2020-01-12", raw_description: "autocat", description: "To be found", value: 12.98, statement: st, user: us, category: uncat, subcategory: unsub)
+    assert_equal("Uncategorized", e1.category.name)
+    assert_equal("Uncategorized", e1.subcategory.name)
 
-    ExpensesHelper.auto_find_category e
-    assert_equal("Bootstrap", e.category.name)
-    assert_equal("Bootstrap", e.subcategory.name)
+    ExpensesHelper.auto_find_category e1
+    assert_equal("Bootstrap", e1.category.name)
+    assert_equal("Bootstrap", e1.subcategory.name)
+
+    e2 = Expense.create(date: "2020-01-12", raw_description: "none", description: "Not to be found", value: 12.98, statement: st, user: us, category: uncat, subcategory: unsub)
+    ExpensesHelper.auto_find_category e2
+    assert_equal("Uncategorized", e2.category.name)
+    assert_equal("Uncategorized", e2.subcategory.name)
   end
 end
