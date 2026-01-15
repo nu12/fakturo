a477675d60d13753f2261d93c4dbd8f0464ceb08
nu12 <34694287+nu12@users.noreply.github.com>
2026-01-15 13:58:33 -0500

    Add file processing retry

--- app/controllers/statement_processings_controller.rb
+++ app/controllers/statement_processings_controller.rb
@@ -1,19 +1,11 @@
 class StatementProcessingsController < ApplicationController
   before_action :set_statement_processing
   def update
-    unless @sp.statement.file.attached?
-      redirect_to @sp.statement, alert: "Statement file has already been deleted.", status: :see_other
-      return
+    if @sp.retry
+      redirect_to @sp.statement, notice: "Processing was successfully rescheduled.", status: :found
+    else
+      redirect_to @sp.statement, alert: @sp.errors.where(:base).first.full_message, status: :see_other
     end
-
-    if @sp.has_succeeded
-      redirect_to @sp.statement, alert: "Statement has already been sucessfully processed.", status: :see_other
-      return
-    end
-
-    @sp.update(has_succeeded: nil)
-    StatementProcessingJob.perform_later @sp, Rpc::Client::StatementProcessingRpcClient
-    redirect_to @sp.statement, notice: "Processing was successfully rescheduled.", status: :found
   end
 
   private
--- app/models/statement_processing.rb
+++ app/models/statement_processing.rb
@@ -5,6 +5,24 @@ class StatementProcessing < ApplicationRecord
 
   before_create :set_uuid
 
+  def run
+    StatementProcessingJob.perform_later self, Rpc::Client::StatementProcessingRpcClient
+  end
+
+  def retry
+    unless self.statement.file.attached?
+      errors.add :base, :invalid, message: "Statement file has already been deleted."
+      return false
+    end
+
+    if self.has_succeeded
+      errors.add :base, :invalid, message: "Statement has already been sucessfully processed."
+      return false
+    end
+    update(has_succeeded: nil)
+    run
+  end
+
   private
   def set_uuid
     self.uuid = SecureRandom.uuid
